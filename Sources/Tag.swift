a2476b690245c9eb67373799117cc25777c68994
Liviru <119770778+livfi@users.noreply.github.com>
2023-11-10 23:16:54 +1100

    Implement tags add & remove

--- Sources/Tag.swift
+++ Sources/Tag.swift
@@ -16,11 +16,40 @@ func color(of tagName: String) -> TagColor {
     TagColor.none
 }
 
+extension NSURL {
+    var tags: [String] {
+        get {
+            do {
+                var rsrc: AnyObject?
+                try self.getResourceValue(&rsrc, forKey: .tagNamesKey)
+                if let tagNamesKey = rsrc as? [String] {
+                    return tagNamesKey
+                }
+            } catch {
+                print(error)
+            }
+            return []
+        }
+        set(tagNames) {
+            do {
+                try self.setResourceValue(tagNames, forKey: .tagNamesKey)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    
+    func setTags(_ tagNames: [String]) throws {
+        try self.setResourceValue(tagNames, forKey: .tagNamesKey)
+    }
+}
+
 @main
 struct Tag: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "A utility for managing finder tags",
-        subcommands: [List.self],
+        subcommands: [List.self, Add.self, Remove.self],
         defaultSubcommand: List.self)
 }
 
@@ -40,6 +69,48 @@ extension Tag {
             for name in tagNames {
                 print(name)
             }
+        }
+    }
+    
+    struct Add: ParsableCommand {
+        static var configuration = CommandConfiguration(abstract: "Add tags.")
+        
+        struct Options: ParsableArguments {
+            @Argument(help: "List of tags to add.")
+            var tagNames: [String]
+        }
+        
+        @OptionGroup var tagOptions: Tag.Options
+        @OptionGroup var addOptions: Add.Options
+        
+        mutating func run() throws {
+            let inputURL = NSURL(fileURLWithPath: tagOptions.path)
+            let tags = inputURL.tags + addOptions.tagNames
+            try inputURL.setTags(tags)
+            print("Set tags \(inputURL.tags) to \(inputURL.relativeString)")
+        }
+    }
+    
+    struct Remove: ParsableCommand {
+        static var configuration = CommandConfiguration(abstract: "Remove tags.")
+        
+        struct Options: ParsableArguments {
+            @Argument(help: "List of tags to remove.")
+            var tagNames: [String]
+        }
+        
+        @OptionGroup var tagOptions: Tag.Options
+        @OptionGroup var addOptions: Add.Options
+        
+        mutating func run() throws {
+            let inputURL = NSURL(fileURLWithPath: tagOptions.path)
+            
+            var tags = inputURL.tags
+            tags.removeAll(where: { addOptions.tagNames.contains($0) })
+            
+            try inputURL.setTags(tags)
+            print("Removed tags")
+            print("Current tags: \(tags)")
         }
     }
 }
