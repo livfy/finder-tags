c3a6b83f9256dbca341e93b0feef3235102d7270
Liviru <119770778+livfi@users.noreply.github.com>
2023-11-21 12:43:31 +1100

    Update to work on macOS Ventura

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "finder-tags",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v13)
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
@@ -18,7 +18,8 @@ let package = Package(
             name: "tag",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
+            ],
+            path: "Sources"
         ),
     ]
 )
