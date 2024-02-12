d18256e1cfb24f6ca3c394b92041a4a59d2047b2
Liviru <119770778+livfi@users.noreply.github.com>
2024-02-12 18:14:11 +1100

    Bump up swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8.1
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "finder-tags",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v10_15)
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
